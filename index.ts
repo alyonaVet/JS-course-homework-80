bc368a2b3f5ba599ccebea0b43328cdb62cc2510
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-21 12:27:40 +0600

    added express server setup;

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
