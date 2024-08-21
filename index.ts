96f325c8aecf805d3b3c43280825a4782c3f41cb
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-21 15:36:37 +0600

    added routers categoriesRouter, placesRouter, itemsRouter; added run function;

--- index.ts
+++ index.ts
@@ -1,13 +1,23 @@
 import express from 'express';
 import categoriesRouter from "./routers/categories";
+import placesRouter from "./routers/places";
+import itemsRouter from "./routers/items";
+import fileDb from "./fileDb";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 app.use('/categories', categoriesRouter);
+app.use('/places', placesRouter);
+app.use('/items', itemsRouter);
 
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
+const run = async () => {
+    await fileDb.init();
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    });
+};
+
+run().catch(console.error);
