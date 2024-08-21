6e62babb4c97326a93cfe7e54ae3e8b29ab73faf
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-21 12:47:26 +0600

    added categoriesRouter;

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import categoriesRouter from "./routers/categories";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use('/categories', categoriesRouter);
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
