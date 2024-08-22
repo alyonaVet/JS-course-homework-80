a66482042f7732aacfff3ead9d38f07e852fe554
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-22 13:18:49 +0600

    added post, get requests to itemsRouter;

--- routers/items.ts
+++ routers/items.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import fileDb from "../fileDb";
+import {imagesUpload} from "../multer";
+import {ItemType} from "../types";
 
 const itemsRouter = express.Router();
 
-itemsRouter.get("/", (req, res) => {
-    return res.send("Here are your items: ");
+itemsRouter.get("/", async (req, res) => {
+    const allItems = await fileDb.getItems();
+    return res.send(allItems);
 });
 
 itemsRouter.get("/:id", (req, res) => {
@@ -11,11 +15,24 @@ itemsRouter.get("/:id", (req, res) => {
     return res.send(item_id);
 });
 
-itemsRouter.post("/", (req, res) => {
-    return res.send(req.body);
+itemsRouter.post("/", imagesUpload.single('image'), async (req, res) => {
+    if (!req.body.title || !req.body.category_id || !req.body.place_id) {
+        return res.status(400).send({error: "Title, category and place must be present in the request"});
+    }
+    const item: ItemType = {
+        title: req.body.title,
+        category_id: req.body.category_id,
+        place_id: req.body.place_id,
+        description: req.body.message || null,
+        image: req.file ? req.file.filename : null,
+    };
+
+    const savedItem = await fileDb.addItem(item);
+
+    return res.send(savedItem);
 });
 
-itemsRouter.delete("/:id", (req, res) => {
+itemsRouter.delete("/:id", async (req, res) => {
     const item_id = req.params.id;
     return res.send(item_id);
 });
