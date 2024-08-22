04a4b5fd6aa3426b77b6ff849784fe10092e4879
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-22 09:57:49 +0600

    refactor get, post requests;

--- routers/places.ts
+++ routers/places.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import fileDb from "../fileDb";
+import {CategoryType, PlaceType} from "../types";
 
 const placesRouter = express.Router();
 
-placesRouter.get("/", (req, res) => {
-    return res.send("Here are your places: ");
+placesRouter.get("/", async (req, res) => {
+    const allPlaces = await fileDb.getPlaces();
+
+    return res.send(allPlaces);
 });
 
 placesRouter.get("/:id", (req, res) => {
@@ -11,9 +15,17 @@ placesRouter.get("/:id", (req, res) => {
     return res.send(place_id);
 });
 
-placesRouter.post("/", (req, res) => {
-    return res.send(req.body);
-});
+placesRouter.post("/", async (req, res) => {
+    if (!req.body.title) {
+        return res.status(400).send({error: "Title must be present in the request"});
+    }
+    const place: PlaceType = {
+        title: req.body.title,
+        description: req.body.description || null,
+    }
+    const savedPlace = await fileDb.addPlace(place);
+
+    return res.send(savedPlace);});
 
 placesRouter.delete("/:id", (req, res) => {
     const place_id = req.params.id;
