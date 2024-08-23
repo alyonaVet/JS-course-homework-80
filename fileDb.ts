68454bacce366339dac6c9f92b0e7b831ea4448a
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-23 23:10:59 +0600

    grouped methods by name;

--- fileDb.ts
+++ fileDb.ts
@@ -31,6 +31,23 @@ const fileDb = {
         const category = data.categories.find(category => category.id === id);
         return category || null;
     },
+    async addCategory(category: CategoryType) {
+        const id = crypto.randomUUID();
+        const categoryData = {id, ...category};
+        data.categories.push(categoryData);
+        await this.save();
+        return categoryData;
+    },
+    async deleteCategory(id: string) {
+        const items = await this.getItems();
+        const itemCategory = items.some(item => item.category_id === id);
+
+        if (itemCategory) {
+            throw new Error(`Could not delete category with assigned items.`);
+        }
+        data.categories = data.categories.filter(category => category.id !== id);
+        await this.save();
+    },
     async updateCategory(id: string, newCategoryData: CategoryType) {
         const category = await this.getCategoryById(id);
 
@@ -51,6 +68,23 @@ const fileDb = {
     async getPlaceById(id: string) {
         const place = data.places.find(place => place.id === id);
         return place || null;
+    },
+    async addPlace(place: PlaceType) {
+        const id = crypto.randomUUID();
+        const placeData = {id, ...place};
+        data.places.push(placeData);
+        await this.save();
+        return placeData;
+    },
+    async deletePlace(id: string) {
+        const items = await this.getItems();
+        const itemPlace = items.some(item => item.place_id === id);
+
+        if (itemPlace) {
+            throw new Error(`Could not delete place with assigned items.`);
+        }
+        data.places = data.places.filter(place => place.id !== id);
+        await this.save();
     },
     async updatePlace(id: string, newPlaceData: PlaceType) {
         const place = await this.getPlaceById(id);
@@ -75,6 +109,17 @@ const fileDb = {
         const item = data.items.find(item => item.id === id);
         return item || null;
     },
+    async addItem(item: ItemType) {
+        const id = crypto.randomUUID();
+        const itemData = {id, ...item};
+        data.items.push(itemData);
+        await this.save();
+        return itemData;
+    },
+    async deleteItem(id: string) {
+        data.items = data.items.filter(item => item.id !== id);
+        await this.save();
+    },
     async updateItem(id: string, newItemData: ItemType) {
         const item = await this.getItemById(id);
 
@@ -90,55 +135,9 @@ const fileDb = {
         await this.save();
         return item;
     },
-    async addCategory(category: CategoryType) {
-        const id = crypto.randomUUID();
-        const categoryData = {id, ...category};
-        data.categories.push(categoryData);
-        await this.save();
-        return categoryData;
-    },
-    async addPlace(place: PlaceType) {
-        const id = crypto.randomUUID();
-        const placeData = {id, ...place};
-        data.places.push(placeData);
-        await this.save();
-        return placeData;
-    },
-    async addItem(item: ItemType) {
-        const id = crypto.randomUUID();
-        const itemData = {id, ...item};
-        data.items.push(itemData);
-        await this.save();
-        return itemData;
-    },
-    async deleteCategory(id: string) {
-        const items = await this.getItems();
-        const itemCategory = items.some(item => item.category_id === id);
-
-        if (itemCategory) {
-            throw new Error(`Could not delete category with assigned items.`);
-        }
-        data.categories = data.categories.filter(category => category.id !== id);
-        await this.save();
-    },
-    async deletePlace(id: string) {
-        const items = await this.getItems();
-        const itemPlace = items.some(item => item.place_id === id);
-
-        if (itemPlace) {
-            throw new Error(`Could not delete place with assigned items.`);
-        }
-        data.places = data.places.filter(place => place.id !== id);
-        await this.save();
-    },
-    async deleteItem(id: string) {
-        data.items = data.items.filter(item => item.id !== id);
-        await this.save();
-    },
     async save() {
         return fs.writeFile(fileName, JSON.stringify(data, null, 2));
     }
-
 };
 
 export default fileDb;
