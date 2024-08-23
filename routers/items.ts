1b19b3a152570b60c0cac5ce80bbbc03448b76c5
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-23 10:27:37 +0600

    added get item by id to itemsRouter;

--- routers/items.ts
+++ routers/items.ts
@@ -10,20 +10,29 @@ itemsRouter.get("/", async (req, res) => {
     return res.send(allItems);
 });
 
-itemsRouter.get("/:id", (req, res) => {
+itemsRouter.get("/:id", async (req, res, next) => {
     const item_id = req.params.id;
-    return res.send(item_id);
+    try {
+        const item = await fileDb.getItemById(item_id);
+        if (!item) {
+            return res.status(404).send({message: "Item not found"});
+        }
+        return res.send(item);
+
+    } catch (error) {
+        next(error);
+    }
 });
 
 itemsRouter.post("/", imagesUpload.single('image'), async (req, res) => {
     if (!req.body.title || !req.body.category_id || !req.body.place_id) {
-        return res.status(400).send({error: "Title, category and place must be present in the request"});
+        return res.status(400).send({error: "Title, categoryId and placeId must be present in the request"});
     }
     const item: ItemType = {
         title: req.body.title,
         category_id: req.body.category_id,
         place_id: req.body.place_id,
-        description: req.body.message || null,
+        description: req.body.description || null,
         image: req.file ? req.file.filename : null,
     };
 
@@ -36,7 +45,7 @@ itemsRouter.delete("/:id", async (req, res, next) => {
     const item_id = req.params.id;
     try {
         await fileDb.deleteItem(item_id);
-        return res.status(200).send({ message: "Item was deleted successfully" });
+        return res.status(200).send({message: "Item was deleted successfully"});
     } catch (error) {
         next(error);
     }
