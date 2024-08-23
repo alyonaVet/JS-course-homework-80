3f6320266c7208eba0ead20ec3924fb5bc757360
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-23 22:59:49 +0600

    added put request to placesRouter; refactor requests: added next;

--- routers/places.ts
+++ routers/places.ts
@@ -4,10 +4,13 @@ import {PlaceType} from "../types";
 
 const placesRouter = express.Router();
 
-placesRouter.get("/", async (req, res) => {
-    const allPlaces = await fileDb.getPlaces();
-
-    return res.send(allPlaces);
+placesRouter.get("/", async (req, res, next) => {
+    try {
+        const allPlaces = await fileDb.getPlaces();
+        return res.send(allPlaces);
+    } catch (error) {
+        next(error);
+    }
 });
 
 placesRouter.get("/:id", async (req, res, next) => {
@@ -24,7 +27,7 @@ placesRouter.get("/:id", async (req, res, next) => {
     }
 });
 
-placesRouter.post("/", async (req, res) => {
+placesRouter.post("/", async (req, res, next) => {
     if (!req.body.title) {
         return res.status(400).send({error: "Title must be present in the request"});
     }
@@ -32,9 +35,12 @@ placesRouter.post("/", async (req, res) => {
         title: req.body.title,
         description: req.body.description || null,
     }
-    const savedPlace = await fileDb.addPlace(place);
-
-    return res.send(savedPlace);
+    try {
+        const savedPlace = await fileDb.addPlace(place);
+        return res.send(savedPlace);
+    } catch (error) {
+        next(error);
+    }
 });
 
 placesRouter.delete("/:id", async (req, res, next) => {
@@ -42,6 +48,25 @@ placesRouter.delete("/:id", async (req, res, next) => {
     try {
         await fileDb.deletePlace(place_id);
         return res.status(200).send({message: "Place was deleted successfully"});
+    } catch (error) {
+        next(error);
+    }
+});
+
+placesRouter.put("/:id", async (req, res, next) => {
+    const place_id = req.params.id;
+
+    const newPlaceData: PlaceType = {
+        title: req.body.title,
+        description: req.body.description || null,
+    };
+
+    try {
+        const updatedPlace = await fileDb.updatePlace(place_id, newPlaceData);
+        if (!updatedPlace) {
+            return res.status(404).send({error: "Place not found"});
+        }
+        return res.send(updatedPlace);
     } catch (error) {
         next(error);
     }
