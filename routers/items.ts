3ec00ead90240bc0676963075cbaf0e475673cbe
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-23 23:01:42 +0600

    added put request to itemsRouter; refactor requests: added next;

--- routers/items.ts
+++ routers/items.ts
@@ -5,9 +5,13 @@ import {ItemType} from "../types";
 
 const itemsRouter = express.Router();
 
-itemsRouter.get("/", async (req, res) => {
-    const allItems = await fileDb.getItems();
-    return res.send(allItems);
+itemsRouter.get("/", async (req, res, next) => {
+    try {
+        const allItems = await fileDb.getItems();
+        return res.send(allItems);
+    } catch (error) {
+        next(error);
+    }
 });
 
 itemsRouter.get("/:id", async (req, res, next) => {
@@ -24,7 +28,7 @@ itemsRouter.get("/:id", async (req, res, next) => {
     }
 });
 
-itemsRouter.post("/", imagesUpload.single('image'), async (req, res) => {
+itemsRouter.post("/", imagesUpload.single('image'), async (req, res, next) => {
     if (!req.body.title || !req.body.category_id || !req.body.place_id) {
         return res.status(400).send({error: "Title, categoryId and placeId must be present in the request"});
     }
@@ -35,10 +39,12 @@ itemsRouter.post("/", imagesUpload.single('image'), async (req, res) => {
         description: req.body.description || null,
         image: req.file ? req.file.filename : null,
     };
-
-    const savedItem = await fileDb.addItem(item);
-
-    return res.send(savedItem);
+    try {
+        const savedItem = await fileDb.addItem(item);
+        return res.send(savedItem);
+    } catch (error) {
+        next(error);
+    }
 });
 
 itemsRouter.delete("/:id", async (req, res, next) => {
@@ -50,6 +56,28 @@ itemsRouter.delete("/:id", async (req, res, next) => {
         next(error);
     }
     return res.send(item_id);
+});
+
+itemsRouter.put("/:id", imagesUpload.single('image'), async (req, res, next) => {
+    const item_id = req.params.id;
+
+    const newItemData: ItemType = {
+        title: req.body.title,
+        category_id: req.body.category_id,
+        place_id: req.body.place_id,
+        description: req.body.description || null,
+        image: req.file ? req.file.filename : null,
+    };
+
+    try {
+        const updatedItem = await fileDb.updateItem(item_id, newItemData);
+        if (!updatedItem) {
+            return res.status(404).send({error: "Item not found"});
+        }
+        return res.send(updatedItem);
+    } catch (error) {
+        next(error);
+    }
 });
 
 export default itemsRouter;
